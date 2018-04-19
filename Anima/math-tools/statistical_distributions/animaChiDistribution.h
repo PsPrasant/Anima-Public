ce2bd8f18c1fb9ee67121e853839b3b7eadd4d97
Aymeric Stamm <aymeric@stamm.fr>
2018-04-19 11:46:10 +0200

    Added references to the works implemented.

--- Anima/math-tools/statistical_distributions/animaChiDistribution.h
+++ Anima/math-tools/statistical_distributions/animaChiDistribution.h
@@ -4,16 +4,22 @@
 
 namespace anima
 {
-    
+
+//! Implementation of Koay and Basser, Analytically exact correction scheme for signal extraction from noisy magnitude MR signals, Journal of Magnetic Resonance (2006).
 double LowerBound(const unsigned int N);
-double XiFunction(const double eta, const double sigma, const unsigned int N);
 double XiFunction(const double theta, const unsigned int N);
-double GFunction(const double eta, const double m, const double sigma, const unsigned int N);
 double GFunction(const double theta, const double r, const unsigned int N);
-double FixedPointFinder(const double m, const double sigma, const unsigned int N, const unsigned int maximumNumberOfIterations = 500, const double epsilon = 1.0e-9);
 double FixedPointFinder(const double r, const unsigned int N, const unsigned int maximumNumberOfIterations = 500, const double epsilon = 1.0e-9);
+
+//! Implementation of Koay, Ozarslan and Basser, A signal transformational framework for breaking the noise floor and its applications in MRI, Journal of Magnetic Resonance (2009).
+double XiFunction(const double eta, const double sigma, const unsigned int N);
+double GFunction(const double eta, const double m, const double sigma, const unsigned int N);
+double FixedPointFinder(const double m, const double sigma, const unsigned int N, const unsigned int maximumNumberOfIterations = 500, const double epsilon = 1.0e-9);
+
+//! Implementation of both Koay technique for Rice parameter estimation
 void GetRiceParameters(const std::vector<double> &samples, const std::vector<double> &weights, double &location, double &scale);
-// Compute cumulative distribution function of Rice distribution (look at https://github.com/cscooper/ClusterLib/blob/master/src/MarcumQ.cc for generalized Marcum Q function computation)
+
+//! In-house function for evaluating the cumulative distribution function of a Rice distribution based on rectangle integral approximation of the Marcum Q function.
 double GetRiceCDF(const double x, const double location, const double scale, const unsigned int gridSize = 2000);
     
 } // end of namespace
--- Anima/math-tools/statistical_distributions/animaChiDistribution.hxx
+++ Anima/math-tools/statistical_distributions/animaChiDistribution.hxx
@@ -4,11 +4,9 @@
 #include <animaKummerFunctions.h>
 #include <animaBesselFunctions.h>
 
-#include <itkExceptionObject.h>
-
 namespace anima
 {
-
+    
 inline double factorial(unsigned int x)
 {
     return (x <= 1) ? 1 : x * factorial(x - 1);
@@ -25,14 +23,6 @@ double LowerBound(const unsigned int N)
     return std::sqrt(std::max(insideValue, 0.0));
 }
 
-double XiFunction(const double eta, const double sigma, const unsigned int N)
-{
-    double thetaSq = eta * eta / sigma / sigma;
-    double betaN = std::sqrt(M_PI / 2.0) * anima::double_factorial(2 * N - 1) / (std::pow(2, N - 1) * anima::factorial(N - 1));
-    double tmpVal = betaN * anima::KummerFunction(-thetaSq / 2.0, -0.5, N);
-    return (eta > 600.0 * sigma) ? 1.0 : 2.0 * N + thetaSq - tmpVal * tmpVal;
-}
-
 double XiFunction(const double theta, const unsigned int N)
 {
     double thetaSq = theta * theta;
@@ -42,15 +32,46 @@ double XiFunction(const double theta, const unsigned int N)
     return (theta > 600.0) ? 1.0 : 2.0 * N + thetaSq - tmpVal * tmpVal;
 }
 
-double GFunction(const double eta, const double m, const double sigma, const unsigned int N)
-{
-    double insideValue = m * m + (anima::XiFunction(eta, sigma, N) - 2 * N) * sigma * sigma;
-    return std::sqrt(std::max(insideValue, 0.0));
-}
-
 double GFunction(const double theta, const double r, const unsigned int N)
 {
     double insideValue = anima::XiFunction(theta, N) * (1.0 + r * r) - 2 * N;
+    return std::sqrt(std::max(insideValue, 0.0));
+}
+
+double FixedPointFinder(const double r, const unsigned int N, const unsigned int maximumNumberOfIterations, const double epsilon)
+{
+    if (r <= anima::LowerBound(N))
+        return 0.0;
+    
+    unsigned int counter = maximumNumberOfIterations;
+    
+    double t0 = r - anima::LowerBound(N);
+    double t1 = anima::GFunction(t0, r, N);
+    
+    while (std::abs(t0 - t1) > epsilon)
+    {
+        t0 = t1;
+        t1 = anima::GFunction(t0, r, N);
+        --counter;
+        
+        if (counter == 0)
+            break;
+    }
+    
+    return t1;
+}
+
+double XiFunction(const double eta, const double sigma, const unsigned int N)
+{
+    double thetaSq = eta * eta / sigma / sigma;
+    double betaN = std::sqrt(M_PI / 2.0) * anima::double_factorial(2 * N - 1) / (std::pow(2, N - 1) * anima::factorial(N - 1));
+    double tmpVal = betaN * anima::KummerFunction(-thetaSq / 2.0, -0.5, N);
+    return (eta > 600.0 * sigma) ? 1.0 : 2.0 * N + thetaSq - tmpVal * tmpVal;
+}
+
+double GFunction(const double eta, const double m, const double sigma, const unsigned int N)
+{
+    double insideValue = m * m + (anima::XiFunction(eta, sigma, N) - 2 * N) * sigma * sigma;
     return std::sqrt(std::max(insideValue, 0.0));
 }
 
@@ -79,29 +100,6 @@ double FixedPointFinder(const double m, const double sigma, const unsigned int N
     }
     
     return (delta > 0) ? -t1 : t1;
-}
-
-double FixedPointFinder(const double r, const unsigned int N, const unsigned int maximumNumberOfIterations, const double epsilon)
-{
-    if (r <= anima::LowerBound(N))
-        return 0.0;
-    
-    unsigned int counter = maximumNumberOfIterations;
-    
-    double t0 = r - anima::LowerBound(N);
-    double t1 = anima::GFunction(t0, r, N);
-    
-    while (std::abs(t0 - t1) > epsilon)
-    {
-        t0 = t1;
-        t1 = anima::GFunction(t0, r, N);
-        --counter;
-        
-        if (counter == 0)
-            break;
-    }
-    
-    return t1;
 }
 
 void GetRiceParameters(const std::vector<double> &samples, const std::vector<double> &weights, double &location, double &scale)
@@ -161,5 +159,5 @@ double GetRiceCDF(const double x, const double location, const double scale, con
     
     return resVal;
 }
-
+    
 } // end of namespace anima
