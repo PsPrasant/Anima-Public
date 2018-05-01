14b584e19e2ffec8a022ec239dfc96d1c1451ff7
Aymeric Stamm <aymeric@stamm.fr>
2018-05-01 16:59:57 +0200

    Update functions related to the Chi distribution by using ARB-based special functions.

--- Anima/math-tools/statistical_distributions/animaChiDistribution.hxx
+++ Anima/math-tools/statistical_distributions/animaChiDistribution.hxx
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "animaChiDistribution.h"
-#include <animaKummerFunctions.h>
-#include <animaBesselFunctions.h>
+#include <animaARBBesselFunctions.h>
+#include <animaARBKummerFunctions.h>
 
 namespace anima
 {
@@ -17,43 +17,67 @@ inline double double_factorial(unsigned int x)
     return (x <= 1) ? 1 : x * double_factorial(x - 2);
 }
 
-double LowerBound(const unsigned int N)
+double LowerBound(const unsigned int N, const double k0ValueSq, const double betaNSq)
 {
-    double insideValue = 2.0 * N / anima::XiFunction(0, N) - 1.0;
+    double xiValue = 2.0 * N - betaNSq * k0ValueSq;
+    double insideValue = 2.0 * N / xiValue - 1.0;
     return std::sqrt(std::max(insideValue, 0.0));
 }
 
-double XiFunction(const double theta, const unsigned int N)
+double XiFunction(const double thetaSq, const unsigned int N, const double k1Value, const double betaNSq)
+{
+    double resVal = 2.0 * N + thetaSq - betaNSq * k1Value * k1Value;
+    
+    if (resVal > 1.0)
+    {
+        std::cerr << "The xi function cannot take values greater than 1." << std::endl;
+        std::cout << thetaSq << " " << k1Value << " " << anima::GetKummerM(-thetaSq / 2.0, -0.5, N) << std::endl;
+        exit(-1);
+    }
+    
+    return resVal;
+}
+
+double GFunction(const double thetaSq, const double rSq, const unsigned int N, const double k1Value, const double betaNSq)
+{
+    double insideValue = anima::XiFunction(thetaSq, N, k1Value, betaNSq) * (1.0 + rSq) - 2 * N;
+    return std::sqrt(std::max(insideValue, 0.0));
+}
+
+double KFunction(const double theta, const double r, const unsigned int N, const double betaNSq, double &k1Value)
 {
     double thetaSq = theta * theta;
+    double rSq = r * r;
+    k1Value = anima::GetKummerM(-thetaSq / 2.0, -0.5, N);
+    double k2Value = anima::GetKummerM(-thetaSq / 2.0, 0.5, N + 1);
+    double gValue = anima::GFunction(thetaSq, rSq, N, k1Value, betaNSq);
+    double num = gValue * (gValue - theta);
+    double denom = theta * (1.0 + rSq) * (1.0 - betaNSq / (2.0 * N) * k1Value * k2Value) - gValue;
+    return theta - num / denom;
+}
+
+double FixedPointFinder(const double r, const unsigned int N, double &k1Value, const unsigned int maximumNumberOfIterations, const double epsilon)
+{
+    double k0Value = anima::GetKummerM(0.0, -0.5, N);
+    double k0ValueSq = k0Value * k0Value;
     double betaN = std::sqrt(M_PI / 2.0) * anima::double_factorial(2 * N - 1) / (std::pow(2, N - 1) * anima::factorial(N - 1));
-    double tmpVal = betaN * anima::KummerFunction(-thetaSq / 2.0, -0.5, N);
+    double betaNSq = betaN * betaN;
     
-    return (theta > 600.0) ? 1.0 : 2.0 * N + thetaSq - tmpVal * tmpVal;
-}
-
-double GFunction(const double theta, const double r, const unsigned int N)
-{
-    double insideValue = anima::XiFunction(theta, N) * (1.0 + r * r) - 2 * N;
-    return std::sqrt(std::max(insideValue, 0.0));
-}
-
-double FixedPointFinder(const double r, const unsigned int N, const unsigned int maximumNumberOfIterations, const double epsilon)
-{
-    if (r <= anima::LowerBound(N))
+    double lb = anima::LowerBound(N, k0ValueSq, betaNSq);
+    if (r <= lb)
         return 0.0;
     
     unsigned int counter = maximumNumberOfIterations;
     
-    double t0 = r - anima::LowerBound(N);
-    double t1 = anima::GFunction(t0, r, N);
-    
+    double t0 = r - lb;
+    double t1 = anima::KFunction(t0, r, N, betaNSq, k1Value);
+
     while (std::abs(t0 - t1) > epsilon)
     {
         t0 = t1;
-        t1 = anima::GFunction(t0, r, N);
+        t1 = anima::KFunction(t0, r, N, betaNSq, k1Value);
         --counter;
-        
+
         if (counter == 0)
             break;
     }
@@ -61,24 +85,31 @@ double FixedPointFinder(const double r, const unsigned int N, const unsigned int
     return t1;
 }
 
-double XiFunction(const double eta, const double sigma, const unsigned int N)
+double GFunction2(const double etaSq, const double mSq, const double sigmaSq, const unsigned int N, const double k1Value, const double betaNSq)
 {
-    double thetaSq = eta * eta / sigma / sigma;
-    double betaN = std::sqrt(M_PI / 2.0) * anima::double_factorial(2 * N - 1) / (std::pow(2, N - 1) * anima::factorial(N - 1));
-    double tmpVal = betaN * anima::KummerFunction(-thetaSq / 2.0, -0.5, N);
-    return (eta > 600.0 * sigma) ? 1.0 : 2.0 * N + thetaSq - tmpVal * tmpVal;
-}
-
-double GFunction(const double eta, const double m, const double sigma, const unsigned int N)
-{
-    double insideValue = m * m + (anima::XiFunction(eta, sigma, N) - 2 * N) * sigma * sigma;
+    double insideValue = mSq + (anima::XiFunction(etaSq / sigmaSq, N, k1Value, betaNSq) - 2 * N) * sigmaSq;
     return std::sqrt(std::max(insideValue, 0.0));
 }
 
-double FixedPointFinder(const double m, const double sigma, const unsigned int N, const unsigned int maximumNumberOfIterations, const double epsilon)
+double KFunction2(const double eta, const double m, const double sigma, const unsigned int N, const double betaNSq, double &k1Value)
+{
+    double mSq = m * m;
+    double etaSq = eta * eta;
+    double sigmaSq = sigma * sigma;
+    double thetaSq = etaSq / sigmaSq;
+    k1Value = anima::GetKummerM(-thetaSq / 2.0, -0.5, N);
+    double k2Value = anima::GetKummerM(-thetaSq / 2.0, 0.5, N + 1);
+    double gValue = anima::GFunction2(etaSq, mSq, sigmaSq, N, k1Value, betaNSq);
+    double num = gValue * (gValue - eta);
+    double denom = eta * (1.0 - betaNSq / (2.0 * N) * k1Value * k2Value) - gValue;
+    return eta - num / denom;
+}
+
+double FixedPointFinder2(const double m, const double sigma, const unsigned int N, double &k1Value, const unsigned int maximumNumberOfIterations, const double epsilon)
 {
     unsigned int counter = maximumNumberOfIterations;
     double betaN = std::sqrt(M_PI / 2.0) * anima::double_factorial(2 * N - 1) / (std::pow(2, N - 1) * anima::factorial(N - 1));
+    double betaNSq = betaN * betaN;
     double delta = betaN * sigma - m;
     
     if (delta == 0)
@@ -87,12 +118,12 @@ double FixedPointFinder(const double m, const double sigma, const unsigned int N
     double mCorrected = (delta > 0) ? betaN * sigma + delta : m;
     
     double t0 = mCorrected;
-    double t1 = anima::GFunction(t0, mCorrected, sigma, N);
+    double t1 = anima::KFunction2(t0, mCorrected, sigma, N, betaNSq, k1Value);
     
     while (std::abs(t0 - t1) > epsilon)
     {
         t0 = t1;
-        t1 = anima::GFunction(t0, mCorrected, sigma, N);
+        t1 = anima::KFunction2(t0, mCorrected, sigma, N, betaNSq, k1Value);
         --counter;
         
         if (counter == 0)
@@ -105,7 +136,7 @@ double FixedPointFinder(const double m, const double sigma, const unsigned int N
 void GetRiceParameters(const std::vector<double> &samples, const std::vector<double> &weights, double &location, double &scale)
 {
     unsigned int sampleSize = samples.size();
-    
+    double k1Value = 0;
     double meanValue = 0;
     double sumWeights = 0, sumSqWeights = 0;
     
@@ -131,33 +162,22 @@ void GetRiceParameters(const std::vector<double> &samples, const std::vector<dou
         sigmaValue = std::sqrt(sigmaValue);
         
         double rValue = meanValue / sigmaValue;
-        double thetaValue = FixedPointFinder(rValue, 1);
+        double thetaValue = FixedPointFinder(rValue, 1, k1Value);
+        k1Value = anima::GetKummerM(-thetaValue * thetaValue / 2.0, -0.5, 1);
         
-        scale = sigmaValue / std::sqrt(XiFunction(thetaValue, 1));
+        scale = sigmaValue / std::sqrt(anima::XiFunction(thetaValue * thetaValue, 1, k1Value, M_PI / 2.0));
         location = thetaValue * scale;
         return;
     }
     
-    location = FixedPointFinder(meanValue, scale, 1);
+    location = FixedPointFinder2(meanValue, scale, 1, k1Value);
 }
 
-double GetRiceCDF(const double x, const double location, const double scale, const unsigned int gridSize)
+double GetRiceCDF(const double x, const double location, const double scale)
 {
     double a = location / scale;
     double b = x / scale;
-    
-    double resVal = 0;
-    
-    for (unsigned int i = 0;i < gridSize;++i)
-    {
-        double t = ((double)i / (double)gridSize);
-        double integrand = t * std::exp(-(b * t - a) * (b * t - a) / 2.0) * anima::scaled_bessel_i(0, a * b * t);
-        resVal += integrand;
-    }
-    
-    resVal *= ((b * b) / gridSize);
-    
-    return resVal;
+    return 1.0 - anima::GetMarcumQ(1, a, b);
 }
     
 } // end of namespace anima
